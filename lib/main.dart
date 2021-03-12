82c340b8a77bd592789e032fdb74c1880f097f5c
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2021-03-12 07:06:07 +0700

    Add TextField Bill Amount and Custom Hex Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tip_calculator_app/util/hexcolor.dart';
 
 void main() => runApp(new MaterialApp(
       home: BillSplitter(),
@@ -15,6 +16,8 @@ class _BillSplitterState extends State<BillSplitter> {
   int _personCounter = 1;
   double _billAmount = 0.0;
 
+  Color _purple = HexColor("#6908d6");
+
   @override
   Widget build(BuildContext context) {
     // for responsive size by device
@@ -28,17 +31,55 @@ class _BillSplitterState extends State<BillSplitter> {
           scrollDirection: Axis.vertical,
           padding: EdgeInsets.all(20.5),
           children: [
+            // Container Top Display
             Container(
               width: 150,
-              height: size.height * 0.27,
+              height: size.height * 0.25,
               decoration: BoxDecoration(
-                  color: Colors.purpleAccent.shade100,
+                  color:
+                      _purple.withOpacity(0.2), // Colors.purpleAccent.shade100,
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Total Per Person"),
+                    Text("\$123"),
+                  ],
+                ),
+              ),
+            ),
+
+            // Container Bottom Display
+
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(
+                    color: Colors.blueGrey.shade100,
+                    style: BorderStyle.solid,
+                  ),
                   borderRadius: BorderRadius.circular(12.0)),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Total Per Person"),
-                  Text("\$123"),
+                  TextField(
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    style: TextStyle(color: _purple),
+                    decoration: InputDecoration(
+                      prefixText: "Bill Amount",
+                      icon: Icon(Icons.attach_money),
+                    ),
+                    onChanged: (String value) {
+                      try {
+                        _billAmount = double.parse(value);
+                      } catch (exception) {
+                        _billAmount = 0.0;
+                      }
+                    },
+                  )
                 ],
               ),
             )
